a9c049e16dcb32e61f8415061c529781344a33ea
mizphses <fuminori@mizphses.com>
2023-07-08 00:26:04 +0900

    ogp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,24 @@ export default defineNuxtConfig({
       title: 'みずさわぽーたる',
       meta: [
         { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        // twitter ogp
+        { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 'twitter:site', name: 'twitter:site', content: '@mizphses' },
+        // ogp
+        { hid: 'og:site_name', property: 'og:site_name', content: 'みずさわぽーたる' },
+        { hid: 'og:title', property: 'og:title', content: 'みずさわぽーたる' },
+        { hid: 'og:description', property: 'og:description', content: 'みずさわぽーたる: みずさわ（mizphses）の個人サイト' },
+        { hid: 'og:image', property: 'og:image', content: 'https://mizphs.es/ogp.png' },
+        { hid: 'og:url', property: 'og:url', content: 'https://mizphs.es' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        // favicon
+        { hid: 'favicon', rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { hid: 'apple-touch-icon', rel: 'apple-touch-icon', href: '/apple-touch-icon-180x180.png' },
+        // SEO
+        { hid: 'description', name: 'description', content: 'みずさわぽーたる: みずさわ（mizphses）の個人サイト' },
+        { hid: 'keywords', name: 'keywords', content: 'mizphses,みずさわ,みずさわぽーたる,ポータルサイト'},
+        { hid: 'author', name: 'author', content: 'mizphses' },
       ],
       link: [
         {
